2c3650548eb4d109ac77d33eb5cb56a0782fa4f1
Nesat Nayem <nesatnayem042@gmail.com>
2023-06-11 08:27:13 -0700

    deployed api connect

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:5000"
-  : "production server url";
+  : "https://frightened-threads-colt.cyclic.app";
